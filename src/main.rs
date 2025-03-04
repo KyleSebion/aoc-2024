0ef25fd8503f94c0f91eaf897f2323c476f4c273
KyleSebion <kyle@kylesebion.com>
2025-03-03 22:49:54 -0600

    init2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#![allow(dead_code)]
+
+fn e1() -> &'static str {
+    "\
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############
+"
+}
+fn e1_p1_fastest() -> usize { 84 }
+fn d() -> &'static str { include_str!("input.txt") }
+
+const CHEAT_SIZE: usize = 2;
+
 fn main() {
-    println!("Hello, world!");
+    for l in d().lines() {
+        println!("A {l} A");
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test] fn a() {}
 }
