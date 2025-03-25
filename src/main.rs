22c3351c312af5756ff13d3471ff73e3d4a3f2c4
KyleSebion <kyle@kylesebion.com>
2025-03-24 21:11:39 -0500

    part2 done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 #![allow(dead_code, clippy::let_and_return)]
+
+use std::collections::HashMap;
+use itertools::Itertools;
+
 const fn d() -> &'static str {
     include_str!("input.txt")
 }
@@ -51,8 +55,46 @@ fn get_next_secrets_nth(d: &str, nth: usize) -> Vec<usize> {
 fn get_next_secrets_nth_sum(d: &str, nth: usize) -> usize {
     get_next_secrets_nth(d, nth).into_iter().sum()
 }
+fn get_secret_diffs(s: usize, nth: usize) -> HashMap<(i8, i8, i8, i8), usize> {
+    let mut last = s;
+    (0..nth)
+        .map(|_| {
+            let res = get_next_secret(last);
+            let l10 = last % 10;
+            let r10 = res % 10;
+            let diff = r10 as i8 - l10 as i8;
+            last = res;
+            (diff, r10)
+        })
+        // .inspect(|v| println!("{} ({})", v.1, v.0))
+        .tuple_windows::<(_, _, _, _)>()
+        .fold(HashMap::new(), |mut a, w| {
+            a.entry((w.0.0, w.1.0, w.2.0, w.3.0)).or_insert(w.3.1);
+            a
+        })
+}
+fn print_123_secret_diffs() {
+    for (k, v) in get_secret_diffs(123, 9) {
+        println!("{k:?} {v}");
+    }
+}
+fn get_secrets_diffs(d: &str, nth: usize) -> Vec<HashMap<(i8, i8, i8, i8), usize>> {
+    d.lines()
+        .map(|s| get_secret_diffs(s.parse::<usize>().unwrap(), nth))
+        .collect()
+}
+fn get_best_secrets_diffs(d: &str, nth: usize) -> usize {
+    let diffs = get_secrets_diffs(d, nth);
+    let mut sums = HashMap::new();
+    for hm in diffs {
+        for (k, v) in hm {
+            *sums.entry(k).or_insert(0) += v;
+        }
+    }
+    sums.into_iter().max_by_key(|v| v.1).expect("get_best_secrets_diffs").1
+}
 fn main() {
-    println!("{:?}", get_next_secrets_nth_sum(d(), 2000));
+    println!("{:?}", get_best_secrets_diffs(d(), 2000));
 }
 
 #[cfg(test)]
@@ -87,5 +129,17 @@ mod test {
     #[test]
     fn t_e1_sum() {
         assert_eq!(37327623, get_next_secrets_nth_sum(e1(), 2000))
+    }
+    #[test]
+    fn t_d_sum() {
+        assert_eq!(19927218456, get_next_secrets_nth_sum(d(), 2000))
+    }
+    #[test]
+    fn t_e2_best() {
+        assert_eq!(23, get_best_secrets_diffs(e2(), 2000));
+    }
+    #[test]
+    fn t_d_best() {
+        assert_eq!(2189, get_best_secrets_diffs(d(), 2000));
     }
 }
