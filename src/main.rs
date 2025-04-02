23ecb0ea611a88bf9f0a01e4ff00010572ea7403
KyleSebion <kyle@kylesebion.com>
2025-04-01 22:20:40 -0500

    minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -70,13 +70,7 @@ Program: 0,1,5,4,3,0
 }
 
 fn d() -> &'static str {
-    "\
-Register A: 44348299
-Register B: 0
-Register C: 0
-
-Program: 2,4,1,5,7,5,1,6,0,3,4,2,5,5,3,0
-"
+    include_str!("input.txt")
 }
 
 fn e7() -> &'static str {
